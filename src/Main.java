8bd38e1498d8e9035fa55744ea0f8e6491b5589d
for3e1 <ek581019@gmail.com>
2026-05-23 15:47:56 +0300

    1.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        //Задача 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName  = "Ivanov";
+        String fullName  = firstName + " " + middleName + " " + lastName;
+        System.out.println("Ф.И.О. сотрудника — " + fullName);
+        //Задача 2
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        //Задача 3
+        fullName = new String("Иванов Семён Семёнович");
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName);
 
     }
 }
